d25962baaf860ae7727176aad50aec82389c6432
RishabhJain2018 <rishabhjain2018@gmail.com>
2018-06-10 14:17:16 +0530

    Update results

--- get_results.py
+++ get_results.py
@@ -20,7 +20,6 @@ hit_id = '3F6045TU7EQW6SFN2RZBJ1TWI0499F'
 # We are only publishing this task to one Worker
 # So we will get back an array with one item if it has been completed
 worker_results = mturk.list_assignments_for_hit(HITId=hit_id, AssignmentStatuses=['Submitted'])
-print worker_results
 
 if worker_results['NumResults'] > 0:
    for assignment in worker_results['Assignments']:
